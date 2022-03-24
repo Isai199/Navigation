8565b9d4f1bc26e931ec777ad69a73187854decc
H_ISA <h_isai_gomez@hotmail.com>
2022-03-23 22:26:40 -0600

    Se utilizo el mismo navigation de la clase, se utilizo una clase para mandar los datos como argumentos

--- app/src/main/java/com/navigation/HomeFragment.kt
+++ app/src/main/java/com/navigation/HomeFragment.kt
@@ -1,11 +1,11 @@
 package com.navigation
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
 import androidx.navigation.fragment.findNavController
 import com.navigation.databinding.FragmentHomeBinding
 
@@ -29,14 +29,35 @@ class HomeFragment : Fragment() {
 
 
         val navController = findNavController()
-        binding.btnHomeToInside.setOnClickListener{
+
+
+
+
+
+        binding.btnHomeToProfile.setOnClickListener{
             //Toast.makeText(context,"prueba",Toast.LENGTH_SHORT).show()
             //navController.navigate(R.id.action_homeFragment_to_insideHome)
-            val directions = HomeFragmentDirections.actionHomeFragmentToInsideHome()
+            //val directions = HomeFragmentDirections.actionHomeFragmentToInsideHome()
+
+            val nombre = binding.etNombre.text.toString()
+            val paterno = binding.etPaterno.text.toString()
+            val materno = binding.etMaterno.text.toString()
+            val usuario = binding.etUser.text.toString()
+            val correo = binding.etCorreo.text.toString()
+
+            //val myArray = arrayOf<String>(nombre,paterno,materno,usuario,correo)
+
+            val user = User(nombre,paterno, materno,usuario,correo)
+
+
+            var directions = HomeFragmentDirections.actionHomeFragmentToProfileFragment(user)
+
             navController.navigate(directions)
         }
         return binding.root
     }
+
+
 
 
 }
--- app/src/main/java/com/navigation/ProfileFragment.kt
+++ app/src/main/java/com/navigation/ProfileFragment.kt
@@ -1,23 +1,46 @@
 package com.navigation
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.navArgs
+import com.navigation.databinding.FragmentProfileBinding
 
 
 class ProfileFragment : Fragment() {
 
 
 
+    private lateinit var binding: FragmentProfileBinding
+    // get the arguments from the Registration fragment
+    private val args : ProfileFragmentArgs by navArgs()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile, container, false)
+
+        binding = FragmentProfileBinding.inflate(layoutInflater)
+
+        //val argumentoRecibido = arguments?.getStringArray("arrayArgument")
+        //Log.i("Problema", argumentoRecibido.toString())
+
+        // Receive the arguments in a variable
+        val userDetails = args.user
+
+
+        binding.tvNombre.setText(userDetails.nombre)
+        binding.tvPaterno.setText(userDetails.paterno)
+        binding.tvMaterno.setText(userDetails.materno)
+        binding.tvUser.setText(userDetails.usuario)
+        binding.tvCorreo.setText(userDetails.correo)
+
+
+        return binding.root
     }
 
 
